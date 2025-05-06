3ab9533c4eab20367363bab75ca12a781fd78ad1
Claudix <claudio.sosa.e21.21@gmail.com>
2025-05-06 17:30:40 -0300

    6/05/25

--- Ahora_si_q_si_Objetos_con_wollok/ejemplito_para_el_ejercicio.py
+++ Ahora_si_q_si_Objetos_con_wollok/ejemplito_para_el_ejercicio.py
@@ -9,12 +9,42 @@ mascota:
     hambriento: bool | int
     } <- podemos llevar a cabo los comportamientos.
 """
+
 def Mascota(atributos:dict)-> dict:
     import ComportamientosMascota as mthMascota
-
-
+    usr_want = atributos["dueño"[input("Do you want to...")]]
+    match usr_want:
+        case "comer":
+            mthMascota.Alimentar(atributos)
+        case "jugar":
+            mthMascota.Jugar(atributos)
+            pass
+        case _:
+            mthMascota.Dormir(atributos)
+            pass
+    
 def Estado(atributos:dict)-> dict:
     import ComportamientosClaseEstado as mthEstado
 
-def Mascota(atributos:dict)-> dict:
+def Owner(atributos:dict)-> dict:
     import ComportamientosUser as mthUser
+
+def main():
+    while(True):
+        nombreMascota:str
+        if not(nombreMascota):
+          # nombreMascota = input("")
+           nombre = input("The pet is still waiting its name, what is that?")
+        else:
+            pass
+        pet:dict = Mascota({
+                "name" : nombre,
+                "trick" : [],
+                "owner" : Owner({}),
+                "state" : Estado({})
+            })
+        
+        
+        if(input("Do you want to exit?, if the answer is yes write <exit> else write another one string pls").lower() == "exit"):
+            break
+main()
